66743933bc5480f0603f9246ec4db881dc7c2b9a
Max MX <max@eec.ms>
2016-02-07 12:45:16 -0500

    ee3 version

---
+++ zip/addon.setup.php
@@ -0,0 +1,12 @@
+<?php
+
+return array(
+	'author'      => 'Max Lazar',
+	'author_url'  => 'https://eec.ms/',
+	'name'        => 'MX Zip',
+	'description' => 'MX Zip adds you capability to add files/folders into zipping archive directly from ExpressionEngine 3',
+	'version'     => '4.0.1',
+	'namespace'   => 'Mx\Zip',
+	'settings_exist' => FALSE,
+	'plugin.typography' => TRUE
+);
